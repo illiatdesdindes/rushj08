e0eb657526f73c5f7cd835837cf9f4bf693c4120
votre_pseudo <moi@email.com>
2014-04-13 18:53:34 +0200

    merge adam modif

--- game/controller/init.php
+++ game/controller/init.php
@@ -1,11 +1,14 @@
 <?php
 
-include "../../controller/check_login.php";
 require_once('../model/Game.class.php');
 require_once('../model/ImperialFrigate.class.php');
-$game = new Game([new ImperialFrigate('Hammer justice', 4, 1, Direction::$LEFT, []),new ImperialFrigate('truc mush', 4, 6, Direction::$DOWN, [])]);
+session_start();
+include "../../controller/check_login.php";
 
-
+$game = new Game([
+		new ImperialFrigate('Hammer justice', 4, 1, Direction::$LEFT, []),
+		new ImperialFrigate('truc mush', 4, 6, Direction::$DOWN, [])
+]);
 
 echo json_encode($game->toHash());
 $game->sessionSave();
--- game/controller/mouvement.php
+++ game/controller/mouvement.php
@@ -1,5 +1,7 @@
-<?php session_start();
+<?php 
+
 include "../model/Game.class.php";
+session_start();
 $ret =  ((file_get_contents("php://input")));
 $test = (json_decode($ret, true));
 $test2 = new Game();
