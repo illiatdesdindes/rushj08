c792edd2cacbc2f54aebe36a1836ee6a2431db90
Idriss FTAITI <iftaiti@e1r10p10.42.fr>
2014-04-13 18:50:38 +0200

    css et logincheck sans session start

--- game/menu.php
+++ game/menu.php
@@ -1,4 +1,5 @@
-<?PHP include "../controller/check_login.php"?>
+<?PHP session_start();
+include "../controller/check_login.php"?>
 <html>
 	<head>
 		<meta name="description" content="Rush01 Piscine PHP">
@@ -9,23 +10,25 @@
 		<title>Awesome Starships Battles II</title>
 	</head>
 	<body>
-			<form>
-					<select id="Type de flotte" name="">
-<option value=""></option>
-<option value=""></option>
-<option value=""></option>
-<option value=""></option>
-					</select>
-			<select id="Valeur de flotte" name="">
-<option value=""></option>
-<option value=""></option>
-<option value=""></option>
-<option value=""></option>
-					</select>
-		
-</form>	
+				
 		<ul class="menu-vertical-simple-2">
-				<li class="bouton_gauche"><a href="../game/index.php">LANCER NOUVELLE PARTIE</a></li>
-				<li class="bouton_gauche"><a href="../view/home.php">RETOUR</a></li>
+				<li class="bouton_gauche"><a href="index.php">ACCUEIL</a></li>
+				<li class="bouton_gauche"><a href="../game/menu.php" target="_blank">NOUVELLE PARTIE</a></li>
+				<li class="bouton_gauche"><a href="../view/chat.php">CHAT</a></li>
+				<li class="bouton_gauche"><a href="../controller/ChangePwd.class.php">MODIF MDP</a></li>
+				<li class="bouton_gauche"><a href="../controller/deco.php">DECO</a></li>
+		</ul>
+		<form name="game_init" action="" method="post" >
+			<fieldset>
+				Type de flotte<br /><select id="Type de flotte" name="Type de flotte">
+					<option value="">Flotte Imperiale</option>
+					<option value="">Flotte Orks</option>
+					<option value="">Flotte Necron</option>
+					<option value="">Flotte Tyranid</option>
+					</select><br />
+Valeure de flotte<br/>
+		0<input type="range" name="Valeur de flotte" min="0" max="500">500
+			</fieldset>
+		</form>	
 	</body>	
-
+</html>
--- view/chat.php
+++ view/chat.php
@@ -13,7 +13,7 @@
 	<body>
 			<ul class="menu-vertical-simple-2">
 				<li class="bouton_gauche"><a href="index.php">ACCUEIL</a></li>
-				<li class="bouton_gauche"><a href="../game/index.php">LANCER NOUVELLE PARTIE</a></li>
+				<li class="bouton_gauche"><a href="../game/index.php">NOUVELLE PARTIE</a></li>
 				<li class="bouton_gauche"><a href="../view/chat.php">CHAT</a></li>
 				<li class="bouton_gauche"><a href="../controller/ChangePwd.class.php">MODIF MDP</a></li>
 				<li class="bouton_gauche"><a href="../controller/deco.php">DECO</a></li>
